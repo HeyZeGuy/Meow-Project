f0773e0fdd4d8d32049ed54f07b9f6ef91e88268
Guy Shalom <guyshalom248@gmail.com>
2025-04-24 21:03:57 +0300

    Add custom GetWalkDirectionX() to player script, supports controller slow walk speeds

--- res/scripts/Player.cs
+++ res/scripts/Player.cs
@@ -4,9 +4,11 @@ using System;
 public partial class Player : CharacterBody2D
 {
 	[Export]
-	public float Speed = 300.0f;
+	public float Speed = 350.0f;
 	[Export]
-	public float JumpVelocity = -400.0f;
+	public float JumpVelocity = -500.0f;
+	[Export]
+	public float SlowWalkMultiplire = 0.5f;
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -25,11 +27,11 @@ public partial class Player : CharacterBody2D
 		}
 
 		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
-		if (direction != Vector2.Zero)
+		float walkDirectionX = GetWalkDirectionX();
+
+		if (walkDirectionX != 0)
 		{
-			velocity.X = direction.X * Speed;
+			velocity.X = walkDirectionX * Speed;
 		}
 		else
 		{
@@ -38,5 +40,32 @@ public partial class Player : CharacterBody2D
 
 		Velocity = velocity;
 		MoveAndSlide();
+	}
+
+
+
+	// Returns direction in x axis, -1/0/1.
+	private float GetWalkDirectionX()
+	{
+		float directionX = Input.GetAxis("move_left", "move_right");
+		bool walkSlowly = false;
+		
+		if (directionX != 0) { 
+			if ( directionX < -.325 || .325 < directionX){ 
+				// Normal walk speed.
+			} else {
+				// Slow walk speed.
+				walkSlowly = true; 
+			}
+
+			directionX /= Mathf.Abs(directionX); // Any value from 0 to 1 will be pinned to 1.
+			
+			// Apply slow walk speed.
+			if (walkSlowly){ 
+				directionX *= SlowWalkMultiplire; 
+			}
+		}
+
+		return directionX;
 	}
 }
